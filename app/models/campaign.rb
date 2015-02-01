811ab43db6065617ab5caa64f0126d2b00139f6d
Kunpeng Liu <kliu102@gmail.com>
2015-01-31 17:37:43 -0800

    Adding uuid create

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,4 @@
 class Campaign < ActiveRecord::Base
   include ActiveUUID::UUID
+  after_initialize {self.uuid = self.create_uuid if self.uuid.nil? }
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,11 +1,9 @@
 class Merchant < ActiveRecord::Base
-  after_initialize :create_unique_identifier
+  include ActiveUUID::UUID
+  after_initialize {self.uuid = self.create_uuid if self.uuid.nil? }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  def create_unique_identifier
-    self.uuid = SecureRandom.uuid if self.uuid.nil?
-  end
 end
