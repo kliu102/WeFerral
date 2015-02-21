a090c3abd4ec6887bf61900595543bac1ad8a379
Kunpeng Liu <kliu102@gmail.com>
2015-02-21 09:49:46 -0800

    fixing review image

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,6 +4,7 @@ module HomeHelper
     end
 
     def campaign_image_path(campaign)
-        "/#{campaign.master_image.image.path}" if campaign.present?
+        # "/#{campaign.master_image.image.path}" if campaign.present?
+        campaign.master_image.image.url if campaign.present?
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
     # config.action_view.raise_on_missing_translations = true
     config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
     Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
-    config.paperclip_defaults = {:storage => :fog, :fog_credentials => {:provider => "Local", :local_root => "#{Rails.root}/public"}, :fog_directory => "", :fog_host => "localhost"}
+    config.paperclip_defaults = {:storage => :fog, :fog_credentials => {:provider => "Local", :local_root => "#{Rails.root}/public"}, :fog_directory => "", :fog_host => Rails.application.routes.default_url_options[:host]}
     Paperclip.options[:command_path] = '/usr/local/bin/'
 
     config.after_initialize do
